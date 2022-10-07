f2099c3439947cb9711202d446a5e8cbfda5aa03
mcleodtech <52888334+mcleodtech@users.noreply.github.com>
2022-10-07 16:40:26 -0500

    Remove Book Selector File

--- src/app/state/books.selectors.ts
+++
@@ -1,4 +0,0 @@
-import { createSelector, createFeatureSelector } from '@ngrx/store';
-import { InventoryInterface } from '../models/inventory.model';
-
-export const selectInventory = createFeatureSelector<InventoryInterface>('inventory')
--- src/app/state/inventory.selectors.ts
+++ src/app/state/inventory.selectors.ts
@@ -1,4 +1,4 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
-
 import { InventoryInterface } from '../models/inventory.model';
 
+export const selectInventory = createFeatureSelector<InventoryInterface>('inventory')
